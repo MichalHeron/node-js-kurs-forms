1d67de524ec846616e4451fd3c57a3335157460b
Heron <michklak91@gmail.com>
2023-04-17 21:46:27 +0200

    password hashing added

--- app/db/models/user.js
+++ app/db/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt') //biblitoeka do hashowania hasel
 const { validateEmail } = require('../validators')
 
 const userSchema = new Schema({
@@ -16,6 +17,20 @@ const userSchema = new Schema({
 		required: true,
 		minLength: [4, 'haslo powinno zawierac min 4 znaki'],
 	},
+})
+
+// userSchema.path('password').set(value => {  // nie do konca wlasciwy sposob hashowania poniewaz hashuje rpzed sprawdzaniem ilosci znakow
+// 	const salt = bcrypt.genSaltSync(10) //liczba oznacza ile mocy obliczeniowej ma uzyc - im wiecej tym lepie
+// 	const hash = bcrypt.hashSync(value, salt)
+// 	return hash
+// })
+
+userSchema.pre('save', function (next) {
+	const user = this
+	const salt = bcrypt.genSaltSync(10) //liczba oznacza ile mocy obliczeniowej ma uzyc - im wiecej tym lepie
+	const hash = bcrypt.hashSync(user.password, salt)
+	user.password = hash
+	next()
 })
 
 userSchema.post('save', function (error, doc, next) {
